ca34add1da2d71783c4ec411dc5cfb025518964c
Raguna9 <Dickyuzumaki73@gmail.com>
2023-02-19 02:58:15 +0800

    section 3

---
+++ src/ProfileCard.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+function ProfileCard({ title, handle, image, description }) {
+  return (
+    <div className="card has-background-light">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
